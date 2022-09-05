33e1f5e2e3872d29a1de9f6c45f759ab6977e9a7
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-09-05 23:28:27 +0200

    UP add card list and modal view image

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -17,16 +17,32 @@ interface CardsProps {
 
 export function CardList({ cards }: CardsProps): JSX.Element {
   // TODO MODAL USEDISCLOSURE
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
   // TODO SELECTED IMAGE URL STATE
+  const [imageURL, setImageUR] = useState('');
 
   // TODO FUNCTION HANDLE VIEW IMAGE
+  function handleViewImage(url: string): void {
+    setImageUR(url);
+    onOpen();
+  }
+  console.log(isOpen, imageURL);
 
   return (
     <>
-      {/* TODO CARD GRID */}
+      <SimpleGrid w="100%" columns={[1, null, 2, 3]} spacing="40px">
+        {
+          /* TODO CARD GRID */
+          cards.map(card => (
+            // eslint-disable-next-line react/jsx-no-bind
+            <Card key={card.id} data={card} viewImage={handleViewImage} />
+          ))
+        }
+      </SimpleGrid>
 
       {/* TODO MODALVIEWIMAGE */}
+      <ModalViewImage isOpen={isOpen} onClose={onClose} imgUrl={imageURL} />
     </>
   );
 }

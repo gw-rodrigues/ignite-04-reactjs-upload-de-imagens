098742b8b6d90095c230c57e55b42f59d794b338
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-09-06 00:58:29 +0200

    UP add modal structure and styling modal

--- src/components/Modal/ViewImage.tsx
+++ src/components/Modal/ViewImage.tsx
@@ -20,4 +20,41 @@ export function ModalViewImage({
   imgUrl,
 }: ModalViewImageProps): JSX.Element {
   // TODO MODAL WITH IMAGE AND EXTERNAL LINK
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent
+        w="auto"
+        maxW={900}
+        h="auto"
+        mx={{ sm: 12, md: 'auto' }}
+        my="auto"
+      >
+        <ModalBody padding={0}>
+          <Image src={imgUrl} width="auto" height="auto" maxH={600} />
+        </ModalBody>
+        <ModalFooter
+          backgroundColor="pGray.800"
+          justifyContent="flex-start"
+          roundedBottom={6}
+        >
+          <Link
+            href={imgUrl}
+            isExternal
+            color="pGray.50"
+            fontSize="sm"
+            lineHeight={4}
+            fontWeight="normal"
+            _focus={{
+              outline: 'none',
+            }}
+            transition="filter .2s"
+            _hover={{ filter: 'brightness(0.7)' }}
+          >
+            Abril original
+          </Link>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
 }

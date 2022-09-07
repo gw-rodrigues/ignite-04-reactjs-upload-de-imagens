b407938d3ba59736997df23687283951e0c11b89
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-09-07 16:48:03 +0200

    UP responsive

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -58,7 +58,7 @@ export default function Home(): JSX.Element {
   return (
     <>
       <Header />
-      <Box maxW={1120} px={20} mx="auto" my={20}>
+      <Box maxW={1120} px={{ base: 8, md: 20 }} mx="auto" my={20}>
         <CardList cards={formattedData} />
         {
           /* TODO RENDER LOAD MORE BUTTON IF DATA HAS NEXT PAGE */

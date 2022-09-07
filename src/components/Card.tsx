b78af1d68b418b47d6a21f4cd26542c3afcd9699
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-09-07 16:34:09 +0200

    UP hover transition and useDisclosure

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -6,6 +6,7 @@ import {
   Skeleton,
   SkeletonText,
   Flex,
+  useDisclosure,
 } from '@chakra-ui/react';
 import { useState } from 'react';
 
@@ -23,11 +24,20 @@ interface CardProps {
 
 export function Card({ data, viewImage }: CardProps): JSX.Element {
   const [isLoading, setIsLoading] = useState(true);
+  const { isOpen: isHover, onOpen, onClose } = useDisclosure();
 
   return (
-    <Box key={data.ts} borderRadius="md" bgColor="pGray.800">
+    <Box
+      key={data.ts}
+      borderRadius="md"
+      bgColor="pGray.800"
+      onMouseEnter={onOpen}
+      onMouseLeave={onClose}
+      filter={isHover ? 'contrast(115%)' : 'none'}
+      transition="filter .3s"
+    >
       <Skeleton isLoaded={!isLoading}>
-        <Flex h={48} overflow="hidden">
+        <Flex h={48} overflow="hidden" borderTopRadius="md">
           <Image
             src={data.url}
             alt={data.title}
@@ -35,10 +45,11 @@ export function Card({ data, viewImage }: CardProps): JSX.Element {
             w="100%"
             h="auto"
             minH={48}
-            borderTopRadius="md"
             onClick={() => viewImage(data.url)}
             onLoad={() => setIsLoading(false)}
             cursor="pointer"
+            transition="transform .3s"
+            transform={isHover ? 'scale(1.2)' : 'none'}
           />
         </Flex>
       </Skeleton>
